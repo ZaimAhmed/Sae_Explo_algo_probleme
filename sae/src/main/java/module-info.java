f1dbbb6e7054815c24c84cacee39963b70bff226
ZaimAhmed <zaim.absh23@gmail.com>
2026-04-09 11:57:05 +0200

    Maven fait.2

---
+++ sae/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module fr.univorleans.iut45.mathsgraphe {
+    requires javafx.controls;
+    exports fr.univorleans.iut45.mathsgraphe;
+}
---
+++ sae/src/test/java/fr/univ_orleans/iut45/mathsgraphe/AppTest.java
@@ -0,0 +1,5 @@
+package fr.univ_orleans.iut45.mathsgraphe;
+
+public class AppTest {
+    
+}
